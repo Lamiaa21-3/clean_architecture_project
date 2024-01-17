49cca27aef2f9729765a4f8878ad789f3690f6c8
Lamiaa21-3 <lamiaagamal213@gmail.com>
2024-01-17 09:56:54 +0200

    add appbar theme

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -1,17 +1,25 @@
 import 'package:clean_arichtecture_project/presentation/color_manager.dart';
+import 'package:clean_arichtecture_project/presentation/fonts_manager.dart';
+import 'package:clean_arichtecture_project/presentation/styles_manager.dart';
 import 'package:clean_arichtecture_project/presentation/values_manager.dart';
 import 'package:flutter/material.dart';
 
-ThemeData getAppTheme (){
+ThemeData getAppTheme() {
   return ThemeData(
-
-    primaryColorDark: ColorManager.darkPrimary ,
-    primaryColorLight:ColorManager.lightPrimary ,
-    disabledColor: ColorManager.grey1,
-    cardTheme: CardTheme(
-      color: ColorManager.white,
-      shadowColor: ColorManager.grey,
-      elevation:  AppSize.s4,
-    )
-  );
+      primaryColorDark: ColorManager.darkPrimary,
+      primaryColorLight: ColorManager.lightPrimary,
+      disabledColor: ColorManager.grey1,
+      cardTheme: CardTheme(
+        color: ColorManager.white,
+        shadowColor: ColorManager.grey,
+        elevation: AppSize.s4,
+      ),
+      appBarTheme: AppBarTheme(
+        color: ColorManager.primary,
+        centerTitle: true,
+        elevation: AppSize.s4,
+        shadowColor: ColorManager.lightPrimary,
+        titleTextStyle:
+            getRegularStyle(color: ColorManager.white, fontSize: FontSize.s16),
+      ));
 }
